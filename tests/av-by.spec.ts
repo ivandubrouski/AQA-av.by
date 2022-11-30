b346d89d1b16e2dcbab8ba6a5fcbc0f4bc22ed22
Ivan Dubrouski <ivan.dubrouski@gmail.com>
2022-11-30 13:17:37 +0300

    first commit.Select via buttons

---
+++ tests/av-by.spec.ts
@@ -0,0 +1,39 @@
+import { test, expect } from "@playwright/test";
+
+test("av.by", async ({ page }) => {
+  //Go to https://av.by/
+  //Verify that page is opened
+
+  await page.goto("https://av.by/");
+  await expect(page).toHaveURL("https://av.by/");
+
+  //Apply filters to find Ford Mustang
+
+  await page.locator("span .dropdown-floatlabel__value").nth(0).click();
+  await page.locator("input.dropdown__input").click();
+  await page.locator("input.dropdown__input").type("ford");
+  await page.keyboard.press("Enter");
+
+  await page
+    .locator("(//span[@class='dropdown-floatlabel__value'])[2]")
+    .click();
+  await page.locator("//*[@data-item-label='Mustang']").click();
+
+  await page.locator("//*[@href='https://cars.av.by/ford/mustang']").click();
+
+  //Choose the car with the highest price
+
+  await page.locator("//span[@class='dropdown-link__value']").click();
+  await page.locator("//button[@data-item-label='дорогие']").click();
+
+  //Open car page and verify it
+
+  const maxPrice = await page
+    .locator(".listing-item__priceusd")
+    .nth(0)
+    .innerText();
+  await page.locator("a.listing-item__link").nth(0).click();
+
+  const price = await page.locator(".card__price-secondary span").innerText();
+  expect(price).toMatch(maxPrice);
+});
